f32d28486ea0ace19699157b422e0a8ed560b9e5
Dario Veliz <velizalexander99@gmail.com>
2025-04-17 16:46:28 -0600

    fix:assets corregido en angular.json

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export var Global = {
   //url base
-  url: 'http://backend.rest/api/',
+  url: 'https://coursesplatformbackend-production.up.railway.app/api/',
   htmlEntities: function (str: any) {
     // Crear un elemento DOM temporal para extraer solo el texto
     const doc = new DOMParser().parseFromString(str, 'text/html');
