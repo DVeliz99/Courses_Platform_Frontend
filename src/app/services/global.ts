e7ba6c52c9bcffa5a6d7cd10ac636f32a611429a
DVeliz99 <velizalexander99@gmail.com>
2025-08-11 21:01:34 -0600

    --amend

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export var Global = {
   //url base
-  url: 'https://web-production-12a8eb.up.railway.app/backend/public/api/',
+  url: 'https://web-production-12a8eb.up.railway.app/api/',
   htmlEntities: function (str: any) {
     // Crear un elemento DOM temporal para extraer solo el texto
     const doc = new DOMParser().parseFromString(str, 'text/html');
