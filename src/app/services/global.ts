bf1a8e12284e26b61b96070e8a2bcb1a74f372f3
DVeliz99 <velizalexander99@gmail.com>
2025-08-11 20:42:50 -0600

    cambio de api

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export var Global = {
   //url base
-  url: 'https://web-production-2cfbd.up.railway.app/',
+  url: 'https://web-production-12a8eb.up.railway.app/backend/public/api/',
   htmlEntities: function (str: any) {
     // Crear un elemento DOM temporal para extraer solo el texto
     const doc = new DOMParser().parseFromString(str, 'text/html');
